51672bd9156ae04ac830da2419e6e1d7b19712a5
Kenneth <kdb.worker.bee@gmail.com>
2026-05-28 14:04:27 -0400

    readme update

--- Crixus/src/index.ts
+++ Crixus/src/index.ts
@@ -6,10 +6,6 @@ const PORT = 3000;
 //GET is the HTTP method
 app.get("/",(req,res) =>{
     res.send("Crixus Fight Organizer.\nGroup Members:\t\nAlex Sturgeon\t\nGurkirat Singh\t\nKenneth Barclay");
-    res.send("\nDescription:\nFight Management System for organizing medieval fight renactments.");
-});
-app.get("/gur",(req,res) =>{
-    res.send("Hello from Typescript Backend by gur");
 });
 
 app.listen(PORT,() =>{
