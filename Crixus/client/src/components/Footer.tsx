c4ce96dae21d72dd9cb60f6d8e732bfbcb8564d3
Alexander-Sturgeon <Alexander.h.sturgeon@outlook.com>
2026-06-11 16:55:04 -0400

    feat: Footer and styles

--- Crixus/client/src/components/Footer.tsx
+++ Crixus/client/src/components/Footer.tsx
@@ -1,9 +1,15 @@
-
+import "../styles/Footer.css";
 
 function Footer(){
 
     return(
-        <footer></footer>
+        <footer className="footer">
+            <p>@Crixus</p>
+            <p>@Alexander Sturgeon</p>
+            <p>@Kenneth Barclay</p>
+            <p>@Gurkirat Singh</p>
+            <p>@Privacy Policy</p>
+        </footer>
     )
 }
 
