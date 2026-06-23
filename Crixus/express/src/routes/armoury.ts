26bb3b4da2566de95f242d7696a974607a27e71f
Alexander-Sturgeon <Alexander.h.sturgeon@outlook.com>
2026-06-23 13:52:02 -0400

    feat: import still broken but I added two gets one for all armor and another for get 3 at random

---
+++ Crixus/express/src/routes/armoury.ts
@@ -0,0 +1,19 @@
+import {Router, Request, Response} from "express";
+
+import pool from "../db";
+
+const router = Router();
+
+//Gets all armour
+router.get("/api/armor", async(_req: Request, res: Response) => {
+    const [rows] = await pool.query("SELECT * FROM armour");
+    res.json(rows);
+});
+
+//Gets 3 pieces of armor at random for the home page so it is different everytime you go to the home page.
+router.get("api/armor/home"), async(_req: Request, res: Response) => {
+    const [threeRows] = await pool.query("SELECT * FROM armour ORDER BY RAND() LIMIT 3") ;
+    res.json(threeRows);
+}
+
+export default router;
