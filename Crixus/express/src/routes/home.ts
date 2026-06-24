3042bb99044f4c7408217320805f120b2d5c5ef5
Kenneth <kdb.worker.bee@gmail.com>
2026-06-24 12:41:01 -0400

    fixed minor bug where .get() was being closed too early.

--- Crixus/express/src/routes/home.ts
+++ Crixus/express/src/routes/home.ts
@@ -6,15 +6,15 @@ const router = Router();
 
 
 //Gets 3 pieces of armor at random for the home page so it is different everytime you go to the home page.
-router.get("api/armor/home"), async(_req: Request, res: Response) => {
+router.get("/api/armor/home", async(_req: Request, res: Response) => {
     const [threeRows] = await pool.query("SELECT * FROM armour ORDER BY RAND() LIMIT 3") ;
     res.json(threeRows);
-}
+})
 
 //Gets 3 pieces of weapons at random for the home page so it is different everytime you go to the home page.
-router.get("api/weapon/home"), async(_req: Request, res: Response) => {
+router.get("/api/weapon/home", async(_req: Request, res: Response) => {
     const [threeRows] = await pool.query("SELECT * FROM weapon ORDER BY RAND() LIMIT 3") ;
     res.json(threeRows);
-}
+})
 
 export default router;
