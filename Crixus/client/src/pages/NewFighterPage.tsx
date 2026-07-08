09a0231ef86f1e0ed6ae02f466a223a1e45ce00c
Alexander-Sturgeon <Alexander.h.sturgeon@outlook.com>
2026-07-07 21:46:05 -0400

    feat: initial form and page creation

--- Crixus/client/src/pages/NewFighterPage.tsx
+++ Crixus/client/src/pages/NewFighterPage.tsx
@@ -1,8 +1,40 @@
+//Styles
+import Fighter from "../data/Fighter";
+import "../styles/NewFighterPage.css";
 
+//Data
+import {useState} from 'react';
 
 function NewFighterPage(){
+    const [formData, setFormData] = useState<Fighter | null>(null);
+
+    function handleChange(){};
+
+    function handleSubmit(e: React.SubmitEvent){
+        // const newFighter = new Fighter(formData);
+    };
+
+
     return(
-        <></>
+        <section>
+            <div className="new-fighter-body">
+                <form onSubmit={handleSubmit}>
+                    <div className="new-fighter-img">
+                        <optgroup>
+
+                        </optgroup>
+                    </div>
+                    <div className="new-figther-form">
+
+                    </div>
+                    <div className="new-fighter-submit">
+
+                    </div>
+                </form>
+                    
+            </div>
+
+        </section>
     )
 }
 
