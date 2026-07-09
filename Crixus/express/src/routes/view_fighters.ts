8fdc32f3fc0f71ada345a4b6465f7b572838940b
Alexander-Sturgeon <Alexander.h.sturgeon@outlook.com>
2026-07-09 12:03:21 -0400

    fix: view fighters route for getting max id

--- Crixus/express/src/routes/view_fighters.ts
+++ Crixus/express/src/routes/view_fighters.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 //Gets all armour
 router.get("/fighters/total", async(_req: Request, res: Response) => {
-    const [rows] = await pool.query("SELECT * FROM Fighters ");
+    const [rows] = await pool.query("SELECT MAX(fighter_id) FROM fighter");
     res.json(rows);
 });
 
