a45bf90942d3f408dc857aefe3e5edc7033c03c9
Alexander-Sturgeon <Alexander.h.sturgeon@outlook.com>
2026-07-09 22:39:42 -0400

    feat: full ui and api implementation of new fighter

--- Crixus/express/src/routes/view_fighters.ts
+++ Crixus/express/src/routes/view_fighters.ts
@@ -1,12 +1,11 @@
 import {Router, Request, Response} from "express";
-
 import pool from "../db";
 
 const router = Router();
 
-//Gets all armour
+//Gets max id fighter
 router.get("/fighters/total", async(_req: Request, res: Response) => {
-    const [rows] = await pool.query("SELECT MAX(fighter_id) FROM fighter");
+    const [rows] = await pool.query("SELECT COALESCE(MAX(fighter_id), 0) AS maxId FROM fighter");
     res.json(rows);
 });
 
--- Crixus/express/src/server.ts
+++ Crixus/express/src/server.ts
@@ -9,6 +9,7 @@ import userSettings from "./routes/user_settings";
 import armoury from  "./routes/armoury";
 import home from "./routes/home";
 import fighters from "./routes/view_fighters";
+import addFighters from "./routes/add_fighters";
 //cors allows requests from other origins
 //express.json parses json request bodies so req.body works. 
 app.use(cors());
@@ -29,8 +30,9 @@ app.use("/armory", armoury);
 
 app.use("/home", home);
 
-app.use("/fighters", fighters)
+app.use("/fighters", fighters);
 
+app.use("/api/newfighters", addFighters);
 //starts server
 app.listen(PORT,() =>{
     console.log(`Server runing on port ${PORT}`);
