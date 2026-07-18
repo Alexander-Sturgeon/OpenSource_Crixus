c6af21f4306aba0e421b80e77219858f7e42e217
Kenneth <kdb.worker.bee@gmail.com>
2026-07-18 17:20:44 -0400

    fully functional fighter update system. Currently hardcoded valuefor the fighter id on viewfighter, but successfully pulls hardcoded fighter_id from db and updates values.

--- Crixus/client/src/pages/EditFighterPage.tsx
+++ Crixus/client/src/pages/EditFighterPage.tsx
@@ -1,7 +1,10 @@
-import "../styles/EditFighterPage.css";
+// import "../styles/EditFighterPage.css";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 
+interface EditPageProp{
+    id: number;
+    onSuccess: () => void; //type means that it returns nothing
+}
 
 interface EditFighter{
     fighter_id: number;
@@ -13,10 +16,10 @@ interface EditFighter{
     constitution: number;
     intelligence: number;
     salary: number;
-    weapon_id: number;
-    armour_id: number;
-    user_id: number;
-    team_id: number;
+    Weapon_weapon_id: number;
+    Armour_armour_id: number;
+    User_user_id: number;
+    Team_team_id: number;
 }
 
 const fightersImg =[
@@ -28,9 +31,107 @@ const fightersImg =[
     {label: "Fighter Pink", path: "/fighters/Fighter6.png"},
 ]
 
-function EditFighterPage(){
-    const {id} = useParams();
+function EditFighterPage({id, onSuccess}: EditPageProp){
+    const[selectedFighter, setSelectedFighter] = useState<EditFighter | null>();
+    const pointPool = 53 - (selectedFighter?.strength ?? 0) - (selectedFighter?.dexterity ?? 0) - (selectedFighter?.constitution ?? 0) - (selectedFighter?.intelligence ?? 0);
 
+    function ChangeStat(key: "strength" | "dexterity" | "constitution" | "intelligence" ,value: string){
+        if(!selectedFighter) return;
+        const remaining = (53 - pointPool) - selectedFighter[key];
+        const hardCap = Math.min(Number(value) || 0, 20,53 - remaining);
+        setSelectedFighter({... selectedFighter, [key]: Math.max(0, hardCap)});
+    }
+
+    //fetches fighter from db to populate stats and such for editing
+    async function GetFighter(){
+        try{
+            console.log("Attempt to GET fighter");
+            const res = await
+            fetch(`http://localhost:3000/api/editFighters/get/${id}`, {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json"
+                }
+            });
+            const data = await res.json();
+            setSelectedFighter(data);
+        }catch(err){
+            console.log("There was an error getting fighter: ", err);
+        }
+    }
+
+    useEffect(() => {GetFighter()}, [id])
+
+    async function HandleSubmit(event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault(); //stops the browsers default page reload
+        if(!selectedFighter) return; //null check
+        const ok = await
+        EditFighter(selectedFighter); if (ok) onSuccess();
+    }
+
+    return(
+        <section>                
+            <h1>Edit Fighter</h1>
+            <form onSubmit={HandleSubmit}>
+                <div className="edit_appearnace">
+                    {/* edit appearance section */}
+                    <label htmlFor="edit_fighter_image">Fighter Appearance: </label>
+                    <select value={selectedFighter?.appearance} onChange={(event) => {if (!selectedFighter) return;setSelectedFighter({...selectedFighter, appearance: event.target.value})}}>
+                        {fightersImg.map((img) => (
+                            <option key={img.label} value={img.path}>
+                                {img.label}
+                            </option>
+                        ))}
+                    </select>
+                    <img className="fighter-preview" src={selectedFighter?.appearance} alt="Selected Fighters Appearance"></img>
+                </div>
+
+                <div className="edit_firstname">
+                    {/* first name input  */}
+                    <label htmlFor="first_name_input">First Name: </label>
+                    <input type="text" id="first_name_input" name="first_name_input" value={selectedFighter?.first_name} onChange={(event) => {if (!selectedFighter) return; setSelectedFighter({...selectedFighter, first_name: event.target.value})}} required />
+                </div>
+
+                <div className="edit_lastname">
+                    {/* last name input */}
+                    <label htmlFor="last_name_input">Last Name: </label>
+                    <input type="text" id="last_name_input" name="last_name_input" value={selectedFighter?.last_name} onChange={(event) => {if(!selectedFighter) return; {setSelectedFighter({...selectedFighter, last_name: event.target.value})}}} required/>
+                </div>
+
+                <div className="edit_stats">
+                    <div className="edit_strength">
+                        {/* edit strength */}
+                        <label htmlFor="edit_strength_input">Strength: </label>
+                        <input type="number" id="edit_strength_input" name="edit_strength_input" value={selectedFighter?.strength} onChange={(event) => ChangeStat("strength", event.target.value)} required/>
+                    </div>
+
+                    <div className="edit_dexterity">
+                        {/* edit dexterity */}
+                        <label htmlFor="edit_dexterity_input">Dexterity: </label>
+                        <input type="number" id="edit_dexterity_input" name="edit_dexterity_input" value={selectedFighter?.dexterity} onChange={(event) => ChangeStat("dexterity", event.target.value)} required/>
+                    </div>
+
+                    <div className="edit_constitution">
+                        {/* edit constitution  */}
+                        <label htmlFor="edit_constitution_input">Constitution</label>
+                        <input type="number" id="edit_constitution_input" name="edit_constitution_input" value={selectedFighter?.constitution} onChange={(event) => ChangeStat("constitution", event.target.value)} required/>
+                    </div>
+
+                    <div className="edit_intelligence">
+                        {/* edit intelligence  */}
+                        <label htmlFor="edit_intelligence_input">Intelligence</label>
+                        <input type="number" id="edit_intelligence_input" name="edit_intelligence_input" value={selectedFighter?.intelligence} onChange={(event) => ChangeStat("intelligence", event.target.value)} required/>
+                    </div>
+                </div>
+
+                <div className="submit_button">
+                    <button type="submit">
+                        Edit Fighter    
+                    </button>
+                </div>
+            </form>
+        </section>
+    )
 }
 
 //put async/await that actually updates the entry
@@ -49,8 +150,10 @@ async function EditFighter(updated_fighter: EditFighter){
         if(!res.ok) throw new Error(`Server responded ${res.status}`);
         const data = await res.json();
         console.log("Updated fighter, id: ", data.fighter_id);
+        return true;
     } catch (err) {
         console.log("There was an error updating fighter: ", err);
+        return false;
     }
 }
 
--- Crixus/express/src/routes/edit_fighters.ts
+++ Crixus/express/src/routes/edit_fighters.ts
@@ -1,13 +1,14 @@
 import {Router, Request, Response} from "express";
 import pool from "../db";
 import {ResultSetHeader} from "mysql2/promise";
+import { RowDataPacket } from "mysql2/promise";
 
 const router = Router()
 
 router.put("/edit/:id", async(req: Request, res: Response) => {
     try{
         const {
-            first_name, last_name, appearance, strength, dexterity, constitution, intelligence, salary, weapon_id, armour_id, user_id, team_id
+            first_name, last_name, appearance, strength, dexterity, constitution, intelligence, salary, Weapons_weapon_id, Armour_armour_id, User_user_id, Team_team_id
         } = req.body;
         const fighter_id = req.params.id;
 
@@ -26,12 +27,26 @@ router.put("/edit/:id", async(req: Request, res: Response) => {
                  Armour_armour_id = ?,
                  User_user_id = ?,
                  Team_team_id = ?
-             WHERE fighter_id = ?`,[first_name, last_name, appearance, strength, dexterity, constitution, intelligence, salary, weapon_id, armour_id, user_id, team_id, fighter_id]
+             WHERE fighter_id = ?`,[first_name, last_name, appearance, strength, dexterity, constitution, intelligence, salary, Weapons_weapon_id, Armour_armour_id, User_user_id, Team_team_id, fighter_id]
         );
         res.status(200).json({fighter_id: fighter_id})
     } catch(err){
         console.error("API FAILED to edit fighter: ", err)
         res.status(500).json({error: "Failed to editFighter."})
+    }
+})
+
+router.get("/get/:id", async(req: Request, res: Response) => {
+    try{
+        const fighter_id = req.params.id;
+        const[rows] = await pool.query<RowDataPacket[]>(
+            `SELECT * FROM fighter 
+             WHERE fighter_id = ?`, [fighter_id]
+        );
+        res.status(200).json(rows[0])
+    }catch(err){
+        console.error("API FAILED to get fighter: ", err)
+        res.status(500).json({error: "Failed to getFighter"})
     }
 })
 
